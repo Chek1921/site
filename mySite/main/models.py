7285ad084c052ab59df01d75123497713780ea8b
BuildTools <unconfigured@null.spigotmc.org>
2023-02-19 04:38:07 +0600

    QWE

--- mySite/main/models.py
+++ mySite/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, User
 from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
@@ -25,22 +25,44 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, username, password, email, **extra_fields):
+    def create_user(self, username, password, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
-
-        if not email:
-            raise ValueError(_("The Email must be set"))
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+        
+        user = self.model(username = username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-class CustomUser(User):
-    address = models.CharField("Адрес", max_length=100, null=False, blank=False)
+    def create_superuser(self, username, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+
+
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(username, password, **extra_fields)
+    
+
+class CustomUser(AbstractUser):
+
+    address = models.CharField("Адрес", max_length=100, null=False, blank=False, unique=True)
     district = models.CharField("Район", max_length=100, null=False, blank=False)
     allows = models.CharField("Разрешение", max_length=1, null=False, blank=False)
+    
+    email = models.EmailField(("email address"),null=False, blank=False, unique=True)
+
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'main',
 ]
 
+AUTH_USER_MODEL = 'main.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
